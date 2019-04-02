12c92eb1082d4f6813805c8844b0e4a10ba035ce
Sujeet <sujeet@infinteit.biz>
2019-04-02 20:02:31 +0530

    Replaced Picasso with Glide

---
+++ app/src/main/java/com/app/utils/MyAppGlideModule.java
@@ -0,0 +1,21 @@
+package com.app.utils;
+
+import android.content.Context;
+
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.DecodeFormat;
+import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.request.RequestOptions;
+
+@GlideModule
+public class MyAppGlideModule extends AppGlideModule {
+    @Override
+    public void applyOptions(Context context, GlideBuilder builder) {
+        // Glide default Bitmap Format is set to RGB_565 since it
+        // consumed just 50% memory footprint compared to ARGB_8888.
+        // Increase memory usage for quality with:
+
+        builder.setDefaultRequestOptions(new RequestOptions().format(DecodeFormat.PREFER_ARGB_8888));
+    }
+}
